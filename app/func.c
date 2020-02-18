40483020b533cd93fb468a24c3a904f6b275b2d2
Kristina Svetova <ksvetova@cs.petrsu.ru>
2020-02-18 22:11:26 +0300

    1.0

--- app/func.c
+++ app/func.c
@@ -1,23 +1,25 @@
 #include <stdio.h>
 #include <math.h>
-int main(){
-    double a, b, c ,x1,x2,d;
-    printf("Введите коэффиценты: a, b, c /n");
-    scanf("%f %f %f", &a, &b, &c);
-    d = b*b - 4*a*c;
-    if (d < 0) {
-        printf("Вещестенных корней нет!/n");
-    }
+int main() {
+	double a, b, c, x1, x2, d;
+	printf("Введите коэффиценты: a, b, c  ");
+	scanf("%lf %lf %lf", &a, &b, &c);
+	d = b * b - 4 * a * c;
+	if (d < 0) {
+		printf("Вещестенных корней нет! ");
+	}
 
-    if (d == 0) {
-       x1= (-b+sqrt(d))/2*a;
-       printf("%f %f", x1);
+	if (d == 0) {
+		x1 = x2 = (-b + sqrt(d)) / (2 * a);
+		printf("%lf", x1);
+		printf("%lf", x2);
 
-    }
 
-    if (d > 0) {
-        x1 =(-b+sqrt(d))/2*a;
-        x2 = (-b-sqrt(d))/2*a;
-    }
-        printf("%f %f", x1,x2);
+	}
+
+	if (d > 0) {
+		x1 = (-b + sqrt(d)) / (2 * a);
+		x2 = (-b - sqrt(d)) / (2 * a);
+	}
+	printf("%lf %lf %lf ", x1, x2, d);
 }
