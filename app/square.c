d8973cc9e79d8e84b2dcb464a3a74b74104562a7
Kristina Svetova <ksvetova@cs.petrsu.ru>
2020-02-23 13:56:54 +0300

    upd

--- app/square.c
+++ app/square.c
@@ -1,7 +1,7 @@
 #include "myfunc.h"
 #include <math.h>
 
-struct pair square(doable a, double c, double b){
+struct pair square(double a, double c, double b){
 	 double d = b*b - 4*a*c;
 	 double x1 = (-b+sqrt(d))/(2*a);
 	 double x2 = (-b-sqrt(d))/(2*a);
