2000a2a5bdc7413044a1268f12cc00c58cf7c192
ksvetova <58701924+ksvetova@users.noreply.github.com>
2020-03-31 14:54:15 +0300

    Update main.c

--- app/main.c
+++ app/main.c
@@ -1,15 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "myfunc.h"
-
 #include "square.h"
-
 
 int main()
 
 {
     printf("Hello World!\n");
     return myfunc(2);
-    return square(1,4,4, NULL);
-
+    
 }
